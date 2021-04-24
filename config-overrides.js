384025e96a9f61e598f52b2ded31dfa5e60f1f30
NguyenThaiTiep <nguyenthaitiep206@gmail.com>
2021-04-24 12:50:57 +0700

    initial app

---
+++ config-overrides.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+module.exports = function override(config, env) {
+  const wasmExtensionRegExp = /\.wasm$/;
+
+  config.resolve.extensions.push(".wasm");
+
+  config.module.rules.forEach((rule) => {
+    (rule.oneOf || []).forEach((oneOf) => {
+      if (oneOf.loader && oneOf.loader.indexOf("file-loader") >= 0) {
+        // Make file-loader ignore WASM files
+        oneOf.exclude.push(wasmExtensionRegExp);
+      }
+    });
+  });
+
+  // Add a dedicated loader for WASM
+  config.module.rules.push({
+    test: wasmExtensionRegExp,
+    include: path.resolve(__dirname, "src"),
+    use: [{ loader: require.resolve("wasm-loader"), options: {} }],
+  });
+
+  config.plugins.push(
+    new MonacoWebpackPlugin({
+      languages: ["json"],
+    })
+  );
+
+  return config;
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
 
 function App() {
+  useEffect(() => {
+    console.log(process.env.NODE_ENV);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>{process.env.REACT_APP_NAME}</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
