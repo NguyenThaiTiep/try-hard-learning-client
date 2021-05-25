dcced9a343633b47f0aa5c601f7c1470e6ac32ed
NguyenThaiTiep <nguyenthaitiep206@gmail.com>
2021-05-25 18:26:48 +0700

    add convert katex

---
+++ src/settings/staticData/katex_const.ts
@@ -0,0 +1,50 @@
+export const KatexConst = [
+  {
+    name: 'Kí tự đơn',
+    data: [
+      "$a'$",
+      "$a''$",
+      '$\\bar{y}$',
+      '$\\widehat{ac}$',
+      '$\\vec{F}$',
+      '$\\overline{AB}$',
+      '$\\overbrace{AB}$',
+      '$\\underbrace{AB}$',
+    ],
+  },
+  {
+    name: 'Kí tự Hy Lạp',
+    data: [
+      '$\\alpha$',
+      '$\\beta$',
+      '$\\gamma$',
+      '$\\delta$',
+      '$\\epsilon$ ',
+      '$\\zeta$',
+      '$\\theta$',
+      '$\\lambda$',
+      '$\\mu$',
+      '$\\xi$',
+      '$\\pi$',
+      '$\\rho$',
+      '$\\sigma$',
+      '$\\tau$',
+      '$\\upsilon$',
+      '$\\phi$',
+      '$\\omega$',
+      '$\\varphi$',
+    ],
+  },
+  {
+    name: 'Kí tự đặc biệt khác',
+    data: ['$\\Reals$', '$\\partial$', '$\\text{\\o}$', '$\\text{\\O}$', '$\\N$', '$\\Z$', '$\\R$', '$\\Complex$'],
+  },
+  {
+    name: 'Kí tự toán học',
+    data: ['$\\forall$'],
+  },
+  {
+    name: 'Công thức toán học',
+    data: ['$f(x) = ax + b$', '$\\begin{Bmatrix}a & b \\\\ c & d\\end{Bmatrix}'],
+  },
+];
